ad1438929ed25aa70c0e76e2ac7c0dbd7f709391
PutNick <Nikolai.Klassen@live.com>
2017-07-31 21:27:17 +0200

    My Contribution to the Getting and Cleaning Data

--- run_analysis.R
+++ run_analysis.R
@@ -21,19 +21,25 @@ subject_data <- rbind(subjecttest, subjecttrain)
 ## Extracts only the measurements on the mean and standard deviation for each measurement.
 ## For X
 
-features <- read.table("F:/GitHub/getting_and_cleaning_data/features.txt") ## The Name-Vectore
+features <- read.table("F:/GitHub/getting_and_cleaning_data/features.txt") 
 mean_and_std <- grep("-(mean|std)\\(\\)", features[, 2])
 x_data <- x_data[, mean_and_std]
 names(x_data) <- features[mean_and_std, 2]
 
-## For Y
+## Step 3: read in the activity.txt in order to replace numbers by the categoric variable
 
 activities <- read.table("F:/GitHub/getting_and_cleaning_data/activity_labels.txt")
-y_data <- activities[y_data[, 1], 2]
-names(y_data) <- "activity"
+y_data[, 1] <- activities[y_data[, 1], 2]
+names(y_data) <- c("activity")
 
-## Step 3
+## Step 4: finally label all data and merge it into one tidy data set
 
 names(subject_data) <- "subject"
 all_data <- cbind(x_data, y_data, subject_data)
+
+## Step 5: Create a seperate tidy dataset for each individual and each activity
+
+average_data <- ddply(all_data, c("subject", "activity"), numcolwise(mean))
+
+write.table(average_data, "tidy.txt", row.names = FALSE, quote = FALSE)
 
